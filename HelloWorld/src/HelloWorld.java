1f602a534979eb0bad6de44094dc40df6af894ab
Mikael Hakman <mhakman@dkab.net>
2015-06-12 20:25:52 +0200

    Initial commit.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+public class HelloWorld {
+
+	public static void main (String [] args) {
+		
+		try {
+			
+			HelloWorld hw = new HelloWorld ();
+			
+			hw.run ();
+			
+		} catch (Exception ex) {
+			
+			ex.printStackTrace ();
+			
+			System.exit (1);;
+		}
+	}
+	
+	protected void run () {
+		
+		System.out.println ("Hello World!");
+	}
+}
